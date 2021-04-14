00ef9c0497a0be39af0b7ab1091df61c2ec3f27c
tropicdenis <tropicdenis@gmail.com>
2021-04-14 16:53:46 +0300

    Layout. Lesson 1 stopped on 58.50

---
+++ src/Main/Main.js
@@ -0,0 +1,13 @@
+import React from "react"
+import style from './Main.module.css';
+
+
+function Main() {
+    return (
+        <div className={style.mainBlock}>
+
+        </div>
+    );
+}
+
+export default Main;
